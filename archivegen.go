5a086b6905ae9c066ea365a017ad6882dce07ce6
Tuomas Lähdekorpi <tuomas.lahdekorpi@gmail.com>
2017-07-19 19:40:55 +0300

    main: update flags, cleanup

--- archivegen.go
+++ archivegen.go
@@ -16,65 +16,92 @@ import (
 	"bitbucket.org/multimfi/archivegen/tree"
 )
 
-var buildversion string
+var (
+	buildversion string
+	rootprefix   string
+)
+
+func open(file *string) *os.File {
+	if file == nil {
+		return os.Stdout
+	}
+	r, err := os.OpenFile(
+		*file,
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+		0644,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return r
+}
+
+func getTree(files []string) *tree.Node {
+	r, err := config.FromFiles(files...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tree.Render(r)
+}
+
+func printTree(t *tree.Node) {
+	tw := tabwriter.NewWriter(os.Stdout, 1, 1, 2, ' ', 0)
+	t.Print(rootprefix, tw)
+	tw.Flush()
+}
+
+func getArchive(fmt string, dst io.Writer) archive.Writer {
+	switch fmt {
+	case "cpio":
+		return cpio.NewWriter(dst)
+	case "tar":
+		return tar.NewWriter(dst)
+	}
+
+	log.Fatal("unknown archive format", fmt)
+	return nil
+}
 
 var (
-	flagOut     = flag.String("out", "out.archive", "Output file, empty for stdout.")
-	flagFormat  = flag.String("fmt", "cpio", "Output file format, cpio/tar.")
-	flagPrint   = flag.Bool("print", false, "Do not write outfile, print resolved tree.")
-	flagVersion = flag.Bool("v", false, "Print version.")
+	flagOut     = flag.String("out", "out.archive", "output file")
+	flagFormat  = flag.String("fmt", "cpio", "file format, cpio/tar")
+	flagPrint   = flag.Bool("print", false, "print resolved tree")
+	flagStdout  = flag.Bool("stdout", false, "output to stdout")
+	flagVersion = flag.Bool("version", false, "version")
 )
 
 func main() {
 	flag.Parse()
 	log.SetFlags(log.Lshortfile)
 
+	if *flagStdout {
+		flagOut = nil
+	}
 	if *flagVersion {
 		fmt.Printf("build: %s\nruntime: %s\n", buildversion, runtime.Version())
 		return
 	}
-
 	if flag.NArg() < 1 {
 		log.Fatal("not enough arguments")
 	}
 
-	var (
-		outw io.WriteCloser
-		w    archive.Writer
-	)
+	root := getTree(flag.Args())
 
-	if *flagOut == "" {
-		outw = os.Stdout
-	} else {
-		f, err := os.OpenFile(*flagOut, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		outw = f
-	}
-	defer outw.Close()
-
-	switch *flagFormat {
-	case "cpio":
-		w = cpio.NewWriter(outw)
-	case "tar":
-		w = tar.NewWriter(outw)
-	}
-	defer w.Close()
-
-	m, err := config.FromFiles(flag.Args()...)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	t := tree.Render(m)
 	if *flagPrint {
-		w := tabwriter.NewWriter(os.Stdout, 1, 1, 2, ' ', 0)
-		t.Print("", w)
-		w.Flush()
+		printTree(root)
 		os.Exit(0)
 	}
-	if err := t.Write("", w); err != nil {
-		log.Fatal(err)
+
+	out := open(flagOut)
+	in := getArchive(*flagFormat, out)
+
+	if err := root.Write(rootprefix, in); err != nil {
+		log.Fatal("error: write:", err)
+	}
+	if err := in.Close(); err != nil {
+		log.Fatal("error: archive: close:", err)
+	}
+	if err := out.Close(); err != nil {
+		log.Fatal("error: output: close:", err)
 	}
 }
