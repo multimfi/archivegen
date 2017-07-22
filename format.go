a673372231a37291d85bb9622d863184362efa5e
Tuomas Lähdekorpi <tuomas.lahdekorpi@gmail.com>
2017-07-22 03:20:55 +0300

    format: clarify

--- format.go
+++ format.go
@@ -11,18 +11,26 @@ Format:
     d *dst mode uid gid
 
   Symlink
+    // dst is the filename in archive
+    // 'l to from' = 'from -> to'
+
     l *dst *src uid gid
 
   File
     f *src dst mode uid gid
 
   Recursive
-    // Omitted dst will target archive root.
+    // omitted dst will target archive root
+    // src path is stripped from dst
+
     R *src *dst uid gid
 
   Create
-    // Data is everything after gid + ' ' or '\t'.
-    c *dst mode uid gid *data...
+    // all preceding ' ' and '\n' are stripped
+    // from data and file is '\n' terminated
+    // 'c file - - -	 foo  bar  ' = 'foo  bar  '
+
+    c *dst mode uid gid *data
 
   Elf
     L *elf dst mode uid gid`
